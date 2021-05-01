21fc09434addd788e7c129d7e9cabe652d2bc7f7
azharalifauzi <azharalifauzi@gmail.com>
2021-05-01 11:18:04 +0700

    setup frontend testing

---
+++ client/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  moduleNameMapper: {
+    '^components(.*)$': '<rootDir>/components$1',
+    '^pages(.*)$': '<rootDir>/pages$1',
+    '^hooks(.*)$': '<rootDir>/hooks$1',
+    '^utils(.*)$': '<rootDir>/utils$1',
+    '^assets(.*)$': '<rootDir>/assets$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': 'babel-jest',
+    '^.+\\.svg$': 'jest-svg-transformer',
+  },
+};
