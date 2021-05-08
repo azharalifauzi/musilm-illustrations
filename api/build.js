5ca6c994c8634f32e92c504919a22b51dd63368e
azharalifauzi <azharalifauzi@gmail.com>
2021-05-09 00:42:31 +0700

    ignoring test file while build project

--- api/build.js
+++ api/build.js
@@ -3,14 +3,14 @@
 const { build } = require('estrella');
 const globby = require('globby');
 
-globby('./src/**/*.ts').then((sourceFiles) => {
+globby(['./src/**/*.ts', '!./src/**/*.test.ts']).then((sourceFiles) => {
   build({
     entryPoints: sourceFiles,
     outdir: 'dist/',
     target: 'es2019',
     platform: 'node',
     format: 'cjs',
-    tsconfig: './tsconfig.json',
+    tsconfig: './tsconfig.build.json',
     watch: process.env.NODE_ENV === 'development' || true,
     run: ['node', 'dist/index.js'],
   });
